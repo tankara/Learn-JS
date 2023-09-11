293581d68f3fbaa663be715691df034c08d40c9c
kantara <tan.kara24@gmail.com>
2023-09-11 20:16:33 +0300

    EventListener ve Dinamik oluşturma pekiştirildi

---
+++ elleme.js
@@ -0,0 +1,28 @@
+let touchMe= document.getElementById("touch");
+
+let ellemeBody = document.getElementById("elleme");
+
+touchMe.style.position = "relative";
+
+touchMe.style.padding = "130px 0px 0px 80px";
+
+let myElleme = document.getElementById("elleme");
+
+// Dinamik olarak element değiştirme
+
+// Yeni element
+let uTouched = document.createElement("p");
+
+uTouched.textContent="YOU TOUCHED BRO WTF?!";
+
+uTouched.style.padding="130px 0px 0px 50px";
+
+uTouched.id="touched";
+
+//EventListener ekleme ve Elementleri birbiri ile değiştirme
+
+myElleme.addEventListener("click", function(e){
+
+    ellemeBody.replaceChild(uTouched,touchMe);
+
+});
